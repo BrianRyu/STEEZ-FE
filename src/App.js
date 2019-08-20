9e84a84fcb3092d660c08fd480f187ac4f11a175
brianryu <s.brianryu@gmail.com>
2019-08-20 16:43:06 -0400

    seed file has been updated

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <BrowserRouter>
           <NavBar />
           <br/><br/><br/>
